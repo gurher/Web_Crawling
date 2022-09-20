11284b54bd14e8f544aadcb4c2c9e406adbde7d9
Hyug Her <gurher@ucdavis.edu>
2022-09-20 16:23:17 -0700

    update

--- Jobs/Indeed/ind.py
+++ Jobs/Indeed/ind.py
@@ -1,4 +1,5 @@
 
+from email.mime import base
 from requests import get 
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -6,20 +7,63 @@ from bs4 import BeautifulSoup
 browser = webdriver.Chrome(executable_path= '/Users/edward/Documents/Web_Scraping/Jobs/Indeed/chromedriver')                                                                     
 
  
-search_term = 'python'
-location = 'California'
-page = 0
+def get_page_count(search_term='python', location='California'):
+    base_url = f"https://www.indeed.com/jobs?q={search_term}&l={location}&from=searchOnHP&vjk=de2c067e43d5b964"
 
-base_url = f"https://www.indeed.com/jobs?q={search_term}&l={location}&from=searchOnHP&vjk=9e42edea9c385922"
-
-
-browser.get(base_url)
+    browser.get(base_url)
 
 
-soup = BeautifulSoup(browser.page_source, 'html.parser')
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    
+    
+    pagination = soup.find("nav", {"class":"css-jbuxu0 ecydgvn0"}) 
+    
+    print(pagination)
+    # pagination = soup.find("ul", class_="pagination-list")
+    if pagination == None:
+        return 1
+    pages = pagination.find_all("li", recursive = False)
+    count = len(pages)
+    if count >=5:
+        return 5
+    else:
+        return count
 
-# for a in soup:
-#     print(a.find('ul', class_="jobsearch-resultsList"))
+print(get_page_count('python', 'California'))
 
 
+# def extract_indeed_jobs(search_term='python', location='California'):
 
+#     base_url = f"https://www.indeed.com/jobs?q={search_term}&l={location}&from=searchOnHP&vjk=de2c067e43d5b964"
+
+
+#     browser.get(base_url)
+
+
+#     soup = BeautifulSoup(browser.page_source, 'html.parser')
+#     job_list = soup.find("ul", class_= 'jobsearch-ResultsList')
+#     jobs = job_list.find_all('li', recursive=False)
+
+#     results = []
+#     for job in jobs:
+#         zone = job.find('div', class_="mosaic-zone")
+#         if zone == None:
+#             anchor = job.select_one("h2 a")
+#             title = anchor['aria-label']
+#             link = anchor['href']
+#             company = job.find('span', class_="companyName")
+#             location = job.find("div", class_="companyLocation")
+            
+#             job_data = {
+                
+#                 'link' : f"https://www.indeed.com{link}",
+#                 'company' : company.string,
+#                 'location' : location.string ,
+#                 'position' : title
+                    
+#             }
+#             results.append(job_data)
+            
+#     for result in results:
+#         print(result, "\n//////\n")
+            
